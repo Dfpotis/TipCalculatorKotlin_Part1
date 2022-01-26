031ab859b34ec4bd6d600277c241612ed97c98e4
potisd22 <dominicpotis@gmail.com>
2022-01-26 12:08:28 -0500

    tip calculator fragments

---
+++ app/src/main/java/com/potis/honorsmobileapps/tipcalculatorkotlin_part1/FinalTotalFragment.kt
@@ -0,0 +1,26 @@
+package com.potis.honorsmobileapps.tipcalculatorkotlin_part1
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.potis.honorsmobileapps.tipcalculatorkotlin_part1.databinding.FragmentMainBinding
+
+class FinalTotalFragment : Fragment() {
+    private var _binding: FragmentMainBinding? =null
+    private val binding get()= _binding!!
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        _binding= FragmentMainBinding.inflate(inflater,container,false)
+        val rootView =binding.root
+        return rootView
+
+    }
+    override fun onDestroyView(){
+        super.onDestroyView()
+        _binding =null
+    }
+}
