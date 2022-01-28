994a2da6c5493b937b4d09fb148376be5c33ede7
potisd22 <dominicpotis@gmail.com>
2022-01-28 12:13:11 -0500

    tip calculator fragments

--- app/src/main/java/com/potis/honorsmobileapps/tipcalculatorkotlin_part1/SubtotalFragment.kt
+++ app/src/main/java/com/potis/honorsmobileapps/tipcalculatorkotlin_part1/SubtotalFragment.kt
@@ -5,19 +5,25 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.navigation.findNavController
 import com.potis.honorsmobileapps.tipcalculatorkotlin_part1.databinding.FragmentMainBinding
+import com.potis.honorsmobileapps.tipcalculatorkotlin_part1.databinding.FragmentSubtotalBinding
 
 
 class SubtotalFragment : Fragment() {
-    private var _binding: FragmentMainBinding? =null
+    private var _binding: FragmentSubtotalBinding? =null
     private val binding get()= _binding!!
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        _binding= FragmentMainBinding.inflate(inflater,container,false)
+        _binding= FragmentSubtotalBinding.inflate(inflater,container,false)
+
         val rootView =binding.root
+        binding.butt.setOnClickListener{
+            rootView.findNavController().navigateUp()
+        }
         return rootView
     }
     override fun onDestroyView(){
